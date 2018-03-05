9d0d3a3ac0e3db70b264304d106a8124f8cae468
haozheng <hza89@sfu.ca>
2018-03-05 14:19:00 -0800

    update menuActivity

--- app/src/main/java/com/example/android/teatime/MenuActivity.java
+++ app/src/main/java/com/example/android/teatime/MenuActivity.java
@@ -18,6 +18,9 @@ package com.example.android.teatime;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -28,11 +31,16 @@ import com.example.android.teatime.model.Tea;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements ImageDownloader.DelayerCallback{
 
     Intent mTeaIntent;
 
     public final static String EXTRA_TEA_NAME = "com.example.android.teatime.EXTRA_TEA_NAME";
+
+    // The Idling Resource which will be null in production
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,9 @@ public class MenuActivity extends AppCompatActivity {
         Toolbar menuToolbar = (Toolbar) findViewById(R.id.menu_toolbar);
         setSupportActionBar(menuToolbar);
         getSupportActionBar().setTitle(getString(R.string.menu_title));
+
+        // Get the IdlingResource instance
+        getIdlingResource();
 
         // Create an ArrayList of teas
         final ArrayList<Tea> teas = new ArrayList<>();
@@ -73,6 +84,48 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ImageDownloader.downloadImage(this, MenuActivity.this, mIdlingResource);
+
+    }
+
+    @Override
+    public void onDone(ArrayList<Tea> teas) {
+        // Create a {@link TeaAdapter}, whose data source is a list of {@link Tea}s.
+        // The adapter know how to create grid items for each item in the list.
+        GridView gridview = (GridView) findViewById(R.id.tea_grid_view);
+        TeaMenuAdapter adapter = new TeaMenuAdapter(this, R.layout.grid_item_layout, teas);
+        gridview.setAdapter(adapter);
+
+
+        // Set a click listener on that View
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Tea item = (Tea) adapterView.getItemAtPosition(position);
+                // Set the intent to open the {@link OrderActivity}
+                mTeaIntent = new Intent(MenuActivity.this, OrderActivity.class);
+                String teaName = item.getTeaName();
+                mTeaIntent.putExtra(EXTRA_TEA_NAME, teaName);
+                startActivity(mTeaIntent);
+
+            }
+        });
+    }
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdingResource();
+        }
+        return mIdlingResource;
     }
 
 }
